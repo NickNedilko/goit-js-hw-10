ed6845479b125efc4ecddd8db5c51d4bf8fb432f
NickNedilko <31307217+NickNedilko@users.noreply.github.com>
2022-10-31 11:40:37 +0000

    Deploying to gh-pages from @ NickNedilko/goit-js-hw-10@81742ff8873710cbd06a0b5769dc8438c05449d5 🚀

--- index.a9158c2d.js
+++
@@ -1,2 +0,0 @@
-const e={inputEl:document.querySelector("#search-box")};console.log(e.inputEl);e.inputEl.addEventListener("input",(function(){console.dir(event.currentTarget.value)}));
-//# sourceMappingURL=index.a9158c2d.js.map
