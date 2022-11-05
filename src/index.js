73317093f86e9f932a104c9766b0aefbb7e7ef74
NickNedilko <overlord311089@gmail.com>
2022-11-05 15:23:01 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,30 +13,29 @@ const refs = {
 const DEBOUNCE_DELAY = 300;
 
 refs.inputEl.addEventListener('input', debounce(onInputSearch, DEBOUNCE_DELAY));
-let countryList = '';
+
 
 function onInputSearch (e){
    clearInterface();
 const searchCountry = e.target.value.trim();
-if(searchCountry.length>=1){
-API.fetchCountries(searchCountry).then(renderCountryList).catch(onError);
+if(searchCountry.length >= 1 ){
+// API.fetchCountries(searchCountry).then(renderCountryList).catch(onError);
+fetchCountries(searchCountry);
 }
-}
-
 
 function clearInterface(){
     refs.countryInfo.innerHTML = '';
     refs.countryList.innerHTML = '';
 }
 
-// function fetchCountries(country){
-//     fetch(`https://restcountries.com/v3.1/name/${country}?fields=name,capital,languages,flags,population`).then(response=>{
-//         if(!response.ok){
-//             throw Error(response.statusText);
-//         }
-//     return response.json()
-//     }).then(renderCountryList).catch(onError)
-// }
+function fetchCountries(country){
+    fetch(`https://restcountries.com/v3.1/name/${country}?fields=name,capital,languages,flags,population`).then(response=>{
+        if(!response.ok){
+            throw Error(response.statusText);
+        }
+    return response.json()
+    }).then(renderCountryList).catch(onError)
+}
 
 
 function onError(){
@@ -52,7 +51,7 @@ function renderCountryList(countries){
         if(countries.length === 1){
             makeCountryInfo(country);
         }
-    makeCountryList(country);
+            makeCountryList(country);
         
     });
 }
@@ -61,7 +60,7 @@ function makeCountryList({flags, name}){
     const country = `<li class="country-list__item">
     <img src="${flags.svg}" width=32  alt="${name.official}">
     ${name.official}</li>`;
-return  countryList = refs.countryList.insertAdjacentHTML('beforeend', country);
+return  refs.countryList.insertAdjacentHTML('beforeend', country);
 }
 
 function makeCountryInfo({name, capital, flags, population, languages}){
@@ -72,7 +71,7 @@ function makeCountryInfo({name, capital, flags, population, languages}){
     <p> Population: ${population}</p>
     <p> Languges: ${Object.values(languages)}</p>
     </div>`
-    return countryList = refs.countryInfo.insertAdjacentHTML('beforeend', countryInfo)
+    refs.countryInfo.insertAdjacentHTML('beforeend', countryInfo)
 }
 
 
